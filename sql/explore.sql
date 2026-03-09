e133f375f2fca9b95c7860b034d6f8c218877716
Vishal Petkar <vishpetkar18@gmail.com>
2026-03-09 13:27:09 +0000

    docs: add comments to SQL queries and notebook
    Added explanatory comments to explore.sql queries and 01_sql_exploration.ipynb for clarity and documentation.

--- sql/explore.sql
+++ sql/explore.sql
@@ -1,4 +1,19 @@
+-- Average rating and total ratings per series
+-- sorted by highest rated
 SELECT series, ROUND(AVG(rating), 2) average_rating, SUM(ratings_count) total_ratings_count_per_series
 FROM books 
 GROUP BY series
 ORDER BY average_rating DESC
+
+
+--Books which are the best and worst performers in each series
+SELECT title, series, rating, ROW_NUMBER() OVER (PARTITION BY series ORDER BY rating DESC) as ranking_by_series
+FROM books
+
+-- best performing book in each series
+WITH best_performer AS (
+SELECT title, series, rating, ROW_NUMBER() OVER (PARTITION BY series ORDER BY rating DESC) as ranking_by_series
+FROM books)
+SELECT title, series, rating
+FROM best_performer
+WHERE ranking_by_series = 1
